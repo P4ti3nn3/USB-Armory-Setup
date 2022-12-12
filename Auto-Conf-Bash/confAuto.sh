b12fe375df851ed48a6935715f59d9155fd31303
P4ti3nn3 <115619908+P4ti3nn3@users.noreply.github.com>
2022-12-12 10:33:58 +0100

    Update confAuto.sh

--- Auto-Conf-Bash/confAuto.sh
+++ Auto-Conf-Bash/confAuto.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+sudo chmod 700 ./confFiles/1AsUSB.sh
+./confFiles/1AsUSB.sh
+sudo chmod 700 ./confFiles/2LoadImage.sh
+./confFiles/2LoadImage.sh
+sudo chmod 700 ./confFiles/3NetwConf.sh
+./confFiles/3NetwConf.sh
+sudo chmod 700 ./confFiles/4Access.sh
+./confFiles/4Access.sh
