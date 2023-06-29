d47bf71b6cdb36b8e0c4ba2a7069fafa295c1a85
P4ti3nn3 <115619908+P4ti3nn3@users.noreply.github.com>
2023-06-29 13:13:37 +0200

    Update Auto-conf.cpp

--- Full-Auto-Conf/Auto-conf.cpp
+++ Full-Auto-Conf/Auto-conf.cpp
@@ -41,7 +41,7 @@ void executeCommandAndWait(const std::string& command) {
     }
 
     pclose(pipe);
-    std::cout<<"\033[1;42mOk!\33[0m"<<endl;
+    std::cout<<"\033[1;42m\033[1;37mOk!\33[0m"<<endl;
 
     
 }
